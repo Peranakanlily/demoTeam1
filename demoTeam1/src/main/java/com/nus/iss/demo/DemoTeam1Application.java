4a8824c7efa8f126760398ed615b1859090197b3
Peranakanlily <44936609+Peranakanlily@users.noreply.github.com>
2018-12-11 11:03:45 +0800

    Add files via upload

---
+++ demoTeam1/src/main/java/com/nus/iss/demo/DemoTeam1Application.java
@@ -0,0 +1,12 @@
+package com.nus.iss.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTeam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTeam1Application.class, args);
+	}
+}
